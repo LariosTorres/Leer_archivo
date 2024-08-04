0819ce9f900b4bbc23e3204a72839b16500d3657
Larios <jose.larios.torres@gmail.com>
2024-08-03 20:15:08 -0600

    Creacion de codigo para buscar una palabra en un archivo .txt local

--- Leer_archivo/Program.cs
+++ Leer_archivo/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿namespace Leer_archivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader lector = new StreamReader(@"C:\Users\l1610\Downloads\Final\archivo.txt");
+            string linea;
+            linea = lector.ReadLine();
+
+            int num_linea = 1;
+
+            Console.WriteLine("¿Que palabra buscas?");
+            string palabra_buscada = Console.ReadLine();
+
+            if (linea.Contains(palabra_buscada))
+                Console.WriteLine($"La palabra que buscas esta en la linea {num_linea} y esta en la posision {linea.IndexOf(palabra_buscada) + 1}");
+            else
+                while ((linea = lector.ReadLine()) != null)
+                {
+                    num_linea++;
+                    if (linea.Contains(palabra_buscada))
+                    {
+                        Console.WriteLine($"La palabra que buscas esta en la linea {num_linea} y esta en la posision {linea.IndexOf(palabra_buscada) + 1}");
+                        break;
+                    }
+
+                }
+            lector.Close();
+        }
+    }
+}
